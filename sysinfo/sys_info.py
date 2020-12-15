10b51fb2998c96ad5bc743e300dac537fec7f4fb
Paillomams <paillomams@gmx.com>
2020-12-14 23:36:04 -0500

    initial contribution

---
+++ sysinfo/sys_info.py
@@ -0,0 +1,70 @@
+# Copyright 2020 Aymeric Pallottini
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Report system info
+
+Classes:
+    - SystemInfoSensor: Class that collects system info data
+"""
+import json
+import psutil
+import time
+from core.sensor import Sensor
+
+
+class SysInfoSensor(Sensor):
+    """Collects an publishes systems info"""
+
+    def __init__(self, publishers, params):
+        """Initialize the sensor. This is not a self running sensor so Poll must
+        be a positive number."""
+        super().__init__(publishers, params)
+
+        self.log.info("Configuing System Info Sensor")
+        if self.poll <= 0:
+            raise ValueError(
+                "SystemInfoSensor requires a positive Poll value: " "%s", self.poll
+            )
+
+        self.sys_info_dict = {}
+
+        # Get configuration
+        self.destination = params("Destination")
+        self.info_required = params("InfoToReport").replace(" ", "").strip().split(",")
+        self.log.debug("Info required: " + str(self.info_required))
+        # Kickoff a poll for the configured sensors.
+        self.check_state()
+
+    def disk_usg(self):
+        dsk_usg = {"root": psutil.disk_usage("/")[3]}
+        dsk_usg["home"] = psutil.disk_usage("/home")[3]
+        dsk_usg["var"] = psutil.disk_usage("/var")[3]
+        self.log.debug(str(json.dumps(dsk_usg)))
+        return dsk_usg
+
+    def check_state(self):
+        """Calculate all required system information and request publish"""
+        if "nb_core" in self.info_required:
+            self.sys_info_dict["core_nb"] = psutil.cpu_count(logical=False)
+        if "cpu_per" in self.info_required:
+            self.sys_info_dict["CPU_per"] = psutil.cpu_percent(1)
+        if "uptime" in self.info_required:
+            self.sys_info_dict["uptime"] = time.time() - psutil.boot_time()
+        if "mem_per" in self.info_required:
+            self.sys_info_dict["mem_per"] = psutil.virtual_memory()[2]
+        if "disk_usage" in self.info_required:
+            self.sys_info_dict["disk_usage"] = self.disk_usg()
+
+        self.publish_state()
+        """Publishes the system info data."""
+        self._send(json.dumps(self.sys_info_dict), self.destination)
